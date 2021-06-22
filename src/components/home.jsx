0ee5444e533ea4c8aa7e53ea88e0e45628306435
Katja Fischer <65532051+Katja39@users.noreply.github.com>
2021-06-22 19:21:34 +0200

    Added Carousel in home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,18 +1,43 @@
-import React, {  } from "react";
+import React, { useEffect, useState } from "react";
+import { InputText } from 'primereact/inputtext';
 import { Carousel } from 'primereact/carousel';
 import imageHome from '../images/imageHome.PNG'
+import { Button } from 'primereact/button';
+import axios from "axios";
 
 
-const Home = (props) => {
+
+const Home = () => {
+    const [products, setProducts] = useState('');
+    const [currentPosition, setCurrentPosition] = useState(0);
+
+    const getProductsSmall = async () => {
+        const res = await axios({
+            method: "get",
+            url: "http://localhost:8000/recipes",
+          })
+          setProducts(res.data);
+          console.log(res);
+        };
+    
+        const productTemplate = () => {
+            return (
+                <div className="product-item">
+                    <img src={products[currentPosition].imageurl} width="500"/>   
+                </div>
+            );
+        }
 
     return (
         <div class="home">
-            <img alt="Let's get Cooking" src={imageHome} ></img>
+            <div class="row">
+            <img alt="Let's get Cooking" src={imageHome} onClick={getProductsSmall} ></img>
             <img alt="Bild" src="" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} height="500" ></img>
-        
-        </div>
-    
-
+            </div>
+         <div className="card">
+           <Carousel value={products} itemTemplate={productTemplate} />
+         </div>
+         </div>
     );
 }
 export default Home;
