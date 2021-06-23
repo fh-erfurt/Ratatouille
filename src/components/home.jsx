c158419e8cdd542f85cdbca80f5c62973c1a48a3
Retch Retch <retch100@gmail.com>
2021-06-23 12:58:14 +0200

    Fixed recipe fetch on render

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,32 +6,34 @@ import { Button } from 'primereact/button';
 import axios from "axios";
 
 
-
 const Home = () => {
     const [products, setProducts] = useState('');
     const [currentPosition, setCurrentPosition] = useState(0);
 
-    const getProducts = async () => {
+    useEffect(() => {
+       fetchProducts();
+    }, []);
+
+    const fetchProducts = async () => {
         const res = await axios({
             method: "get",
             url: "http://localhost:8000/recipes",
           })
-          setProducts(res.data);
-          console.log(res);
-        };
+        setProducts(res.data);
+    };
     
-        const productTemplate = () => {
-            return (
-                <div className="product-item">
-                    <img src={products[currentPosition].imageurl} width="500"/>   
-                </div>
-            );
-        }
+    const productTemplate = () => {
+        return (
+            <div className="product-item">
+                <img src={products[currentPosition].imageurl} width="500"/>   
+            </div>
+        );
+    }
 
     return (
         <div class="home">
             <div class="row">
-            <img alt="Let's get Cooking" src={imageHome} onClick={getProducts} ></img>
+            <img alt="Let's get Cooking" src={imageHome} ></img>
             <img alt="Bild" src="" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} height="500" ></img>
             </div>
          <div className="card">
