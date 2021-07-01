f296bd0ad42718df9833f5e96da7706312d5eba9
Dominic0074 <61750782+Dominic0074@users.noreply.github.com>
2021-07-01 19:39:53 +0200

    anfang fav liste

--- src/components/recipesMenu.jsx
+++ src/components/recipesMenu.jsx
@@ -18,7 +18,7 @@ const RecipesMenu = () => {
      }, []);
 
     const   setfavorite = async () => {
-        
+           console.log(recipID)
             const res = await axios({
                 method: "post",
                 url: "https://ratatouilleexpress.retch.duckdns.org/api/recipes/like",
@@ -34,15 +34,18 @@ const RecipesMenu = () => {
      useEffect(() => {
         if (products) {
             products.forEach(product => {
+                console.log(product.id)
                 let allRecipes = recipecards;
-                setRecipID(product.id);
+
+             setRecipID(product.id)
                 allRecipes.push (
-                    <React.Fragment key={product.id}>
+                   
+                    <React.Fragment key={product.id}> 
                         <div class="oneRecipe p-p-1 p-m-lg-3 p-b-lg-3"> 
                         <Card className="p-mb-2" title={product.name} subTitle={<Chip label={product.categories} className="p-mr-2 p-mb-2 custom-chip" />} style={{ width: '25em', height:'30em'}} footer={product.averagetimeinminutes + " Minuten"} header={<div><img alt="pictureRecipe" src={product.imageurl}></img></div>}>
                         <div className = "likeButton"><Button icon="pi pi-heart" className="p-button-rounded p-button-help p-button-text"
-                            onClick={setfavorite} /></div>
-                            {recipID}
+                            onClick={setfavorite,console.log(product.id)} /></div>
+                            
                         </Card>
                         </div>
                     </React.Fragment>
