4877a5f69480a9b688a9872adaeebf39764da871
Lissyflecky <michelle.a.john2001@gmail.com>
2021-07-01 14:42:46 +0200

    likeButton hinzugefügt

--- src/components/recipesMenu.jsx
+++ src/components/recipesMenu.jsx
@@ -1,5 +1,7 @@
 import RecipeCard from './recipeCard';
 import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+import { Chip } from 'primereact/chip';
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import axios from "axios";
 
@@ -19,8 +21,8 @@ const RecipesMenu = () => {
                 allRecipes.push (
                     <React.Fragment key={product.id}>
                         <div class="oneRecipe p-m-1 p-p-1 p-m-lg-3 p-b-lg-3"> 
-                        <Card className="p-mb-2" title={product.name} subTitle={product.categories} style={{ width: '25em', height:'30em' }} footer={product.averagetimeinminutes + " Minuten"} header={<div><img alt="pictureRecipe" src={product.imageurl}></img></div>}>
-                        
+                        <Card className="p-mb-2" title={product.name} subTitle={<Chip label={product.categories} className="p-mr-2 p-mb-2 custom-chip" />} style={{ width: '25em', height:'30em'}} footer={product.averagetimeinminutes + " Minuten"} header={<div><img alt="pictureRecipe" src={product.imageurl}></img></div>}>
+                        <div className = "likeButton"><Button icon="pi pi-heart" className="p-button-rounded p-button-help p-button-text" /></div>
                         </Card>
                         </div>
                     </React.Fragment>
