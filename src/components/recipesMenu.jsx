8a55eee27fa7db3567c9a144f1523b79349ab186
Dominic0074 <61750782+Dominic0074@users.noreply.github.com>
2021-07-01 15:59:25 +0200

    paar changes

--- src/components/recipesMenu.jsx
+++ src/components/recipesMenu.jsx
@@ -5,24 +5,44 @@ import { Chip } from 'primereact/chip';
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import axios from "axios";
 
+
+
 const RecipesMenu = () => {
     const [products, setProducts] = useState(null);
     const [status, setStatus] = useState(404);
     const [recipecards, setRecipeCards] = useState([]);
+    const [recipID, setRecipID] = useState([]);
 
     useLayoutEffect(() => {
         fetchProducts();
      }, []);
 
+    const   setfavorite = async () => {
+        
+            const res = await axios({
+                method: "post",
+                url: "https://ratatouilleexpress.retch.duckdns.org/api/recipes/like",
+                data: {
+                    "accountId": window.$id,
+                    "recipeId": recipID
+                    
+                }
+              }).catch(error => {
+                return { error: error };
+              });
+        }
      useEffect(() => {
         if (products) {
             products.forEach(product => {
                 let allRecipes = recipecards;
+                setRecipID(product.id);
                 allRecipes.push (
                     <React.Fragment key={product.id}>
                         <div class="oneRecipe p-m-1 p-p-1 p-m-lg-3 p-b-lg-3"> 
                         <Card className="p-mb-2" title={product.name} subTitle={<Chip label={product.categories} className="p-mr-2 p-mb-2 custom-chip" />} style={{ width: '25em', height:'30em'}} footer={product.averagetimeinminutes + " Minuten"} header={<div><img alt="pictureRecipe" src={product.imageurl}></img></div>}>
-                        <div className = "likeButton"><Button icon="pi pi-heart" className="p-button-rounded p-button-help p-button-text" /></div>
+                        <div className = "likeButton"><Button icon="pi pi-heart" className="p-button-rounded p-button-help p-button-text"
+                            onClick={setfavorite} /></div>
+                            {recipID}
                         </Card>
                         </div>
                     </React.Fragment>
