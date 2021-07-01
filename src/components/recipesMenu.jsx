84fab3e653833745d3046d4b431cfa3e6a915d62
Lissyflecky <michelle.a.john2001@gmail.com>
2021-07-01 12:32:39 +0200

    Überarbeitung des padding mit spacing
    padding entfernt

--- src/components/recipesMenu.jsx
+++ src/components/recipesMenu.jsx
@@ -18,7 +18,7 @@ const RecipesMenu = () => {
                 let allRecipes = recipecards;
                 allRecipes.push (
                     <React.Fragment key={product.id}>
-                        <div className = 'allRecipes'>
+                        <div class="oneRecipe p-m-1 p-p-1 p-m-lg-3 p-b-lg-3"> 
                         <Card className="p-mb-2" title={product.name} subTitle={product.categories} style={{ width: '25em', height:'30em' }} footer={product.averagetimeinminutes + " Minuten"} header={<div><img alt="pictureRecipe" src={product.imageurl}></img></div>}>
                         
                         </Card>
