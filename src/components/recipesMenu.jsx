2260c37d5fa1a3535a999edc57648f3fc798d310
Lissyflecky <michelle.a.john2001@gmail.com>
2021-07-01 19:58:02 +0200

    Testing

--- src/components/recipesMenu.jsx
+++ src/components/recipesMenu.jsx
@@ -24,7 +24,7 @@ const RecipesMenu = () => {
                 url: "https://ratatouilleexpress.retch.duckdns.org/api/recipes/like",
                 data: {
                     "accountId": window.$id,
-                    "recipeId": recipID
+                    "recipeId": recipID 
                     
                 }
               }).catch(error => {
@@ -35,17 +35,17 @@ const RecipesMenu = () => {
         if (products) {
             products.forEach(product => {
                 console.log(product.id)
+
+                setRecipID(product.id)
                 let allRecipes = recipecards;
 
-             setRecipID(product.id)
+             
                 allRecipes.push (
                    
                     <React.Fragment key={product.id}> 
                         <div class="oneRecipe p-p-1 p-m-lg-3 p-b-lg-3"> 
                         <Card className="p-mb-2" title={product.name} subTitle={<Chip label={product.categories} className="p-mr-2 p-mb-2 custom-chip" />} style={{ width: '25em', height:'30em'}} footer={product.averagetimeinminutes + " Minuten"} header={<div><img alt="pictureRecipe" src={product.imageurl}></img></div>}>
-                        <div className = "likeButton"><Button icon="pi pi-heart" className="p-button-rounded p-button-help p-button-text"
-                            onClick={setfavorite,console.log(product.id)} /></div>
-                            
+                        <div onClick={setfavorite} className = "likeButton"><Button icon="pi pi-heart" className="p-button-rounded p-button-help p-button-text"/></div>
                         </Card>
                         </div>
                     </React.Fragment>
