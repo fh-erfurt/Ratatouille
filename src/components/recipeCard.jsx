5f769e4edbe1e5342ea30ccd375bdc9669aa4bdc
Lissyflecky <michelle.a.john2001@gmail.com>
2021-07-04 18:43:52 +0200

    tests

--- src/components/recipeCard.jsx
+++ src/components/recipeCard.jsx
@@ -28,6 +28,8 @@ const RecipeCard = ({cardProduct}) => {
 
         }
 
+        
+
     const checkUserStatus = () =>{
         if(window.$id === undefined || window.$id !== cardProduct.creatorId)
         {
@@ -35,7 +37,7 @@ const RecipeCard = ({cardProduct}) => {
         }
         else if(window.$id === cardProduct.creatorId)
         {
-            return(<div onClick={editRecipe} className = "recipeButton"><Button icon="pi pi-pencil" className="p-button-rounded p-button-help p-button-text"/></div>);
+            return(<div onClick={editRecipe} className = "recipeButton" ><Button icon="pi pi-pencil" className="p-button-rounded p-button-info p-button-text"/></div>);
         }
     }
 
@@ -43,8 +45,8 @@ const RecipeCard = ({cardProduct}) => {
     
     console.log(cardProduct.creatorId),
     
-    <Card className="p-mb-2" title={cardProduct.name} subTitle={<Chip label={cardProduct.categories} className="p-mr-2 p-mb-2 custom-chip" />} style={{ width: '25em', height:'30em'}} footer={cardProduct.averagetimeinminutes + " Minuten"} header={<div><img alt="pictureRecipe" src={cardProduct.imageurl}></img></div>}>
-        <div className = "button">{checkUserStatus()}</div>
+    <Card className="p-mb-2" title={cardProduct.name} subTitle={<Chip label={cardProduct.categories} className="p-mr-2 p-mb-2 custom-chip" />} style={{ width: '25em', height:'32em'}} footer={<div className = "p-pb-1 ">{cardProduct.averagetimeinminutes + " Minuten"}</div>} header={<div><img alt="pictureRecipe" src={cardProduct.imageurl}></img></div>}>
+        <div className = "p-pb-1 p-pr-1">{checkUserStatus()}</div>
     </Card>
     
     )
