9db109838c048a9d787955e95538f9e4f1f8339a
Katja Fischer <65532051+Katja39@users.noreply.github.com>
2021-07-04 18:02:16 +0200

    Updated home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,34 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { InputText } from 'primereact/inputtext';
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { Carousel } from 'primereact/carousel';
 import imageHome from '../images/imageHome.PNG'
-import { Button } from 'primereact/button';
 import axios from "axios";
+import { Card } from 'primereact/card';
 
 
 const Home = () => {
-    const [products, setProducts] = useState('');
-    const [currentPosition, setCurrentPosition] = useState(0);
+    const [products, setProducts] = useState(null);
+    const [status, setStatus] = useState(404);
+    const [recipecards, setRecipeCards] = useState([]);
+
+    useLayoutEffect(() => {
+        fetchProducts();
+     }, []);
 
     useEffect(() => {
-       fetchProducts();
-    }, []);
+        if (products) {
+            products.forEach(product => {
+                let allRecipes = recipecards;
+                allRecipes.push (
+                    <React.Fragment key={product.id}>
+                        <Card title={product.name} header={<img src={product.imageurl}></img>} style={{ width: '33em'}}/>
+                    </React.Fragment>
+                );
+                setRecipeCards(allRecipes);
+            })
+        }
+     }, [products]);
 
-    const fetchProducts = async () => {
+     const fetchProducts = async () => {
         const res = await axios({
             method: "get",
             url: "https://ratatouilleexpress.retch.duckdns.org/api/recipes",
-          })
+        }).catch(error => {
+            return { error: error };
+        });
         setProducts(res.data);
+        if (res.status === 200) {
+            setStatus(200);
+        }
     };
-    
-    const productTemplate = () => {
-        return (
-            <div className="product-item">
-                <img src={products[currentPosition].imageurl} width="500"/>   
-            </div>
-        );
+  
+    const itemTemplate = (recipes) => {
+      return recipes;
     }
+  
 
     return (
         <div className="home">
@@ -37,7 +53,8 @@ const Home = () => {
             <img alt="Bild" src="" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} height="500" ></img>
             </div>
          <div className="card">
-           <Carousel value={products} itemTemplate={productTemplate} />
+         <Carousel value={recipecards} itemTemplate={itemTemplate} numVisible={3} numScroll={1} circular
+                    autoplayInterval={4000}  />
          </div>
          </div>
     );
